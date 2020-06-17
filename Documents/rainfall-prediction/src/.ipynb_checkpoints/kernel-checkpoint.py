91da7be8eda1c8e6236b77796296fc8c4efd6bfb
sanjanarao2 <sanjanarao540@gmail.com>
2020-06-17 15:39:17 +0530

    First Commit

---
+++ Documents/rainfall-prediction/src/.ipynb_checkpoints/kernel-checkpoint.py
@@ -0,0 +1,572 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+# In[2]:
+
+
+data = pd.read_csv("../data/rainfall_in_india_1901-2015.csv",sep=",")
+data = data.fillna(data.mean())
+data.info()
+
+
+# In[3]:
+
+
+data.head()
+
+
+# In[4]:
+
+
+data.describe()
+
+
+# In[5]:
+
+
+data.hist(figsize=(24,24));
+
+
+# ## Observations
+# - Above histograms show the distribution of rainfall over months.
+# - Observed increase in amount of rainfall over months July, August, September.
+
+# In[6]:
+
+
+data.groupby("YEAR").sum()['ANNUAL'].plot(figsize=(12,8));
+
+
+# ## Observations
+# - Shows distribution of rainfall over years. 
+# - Observed high amount of rainfall in 1950s.
+
+# In[7]:
+
+
+data[['YEAR', 'JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL',
+       'AUG', 'SEP', 'OCT', 'NOV', 'DEC']].groupby("YEAR").sum().plot(figsize=(13,8));
+
+
+# In[8]:
+
+
+data[['YEAR','Jan-Feb', 'Mar-May',
+       'Jun-Sep', 'Oct-Dec']].groupby("YEAR").sum().plot(figsize=(13,8));
+
+
+# ## Observations
+# - The above two graphs show the distribution of rainfall over months.
+# - The graphs clearly shows that amount of rainfall in high in the months july, aug, sep which is monsoon season in India.
+
+# In[9]:
+
+
+data[['SUBDIVISION', 'JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL',
+       'AUG', 'SEP', 'OCT', 'NOV', 'DEC']].groupby("SUBDIVISION").mean().plot.barh(stacked=True,figsize=(13,8));
+
+
+# In[10]:
+
+
+data[['SUBDIVISION', 'Jan-Feb', 'Mar-May',
+       'Jun-Sep', 'Oct-Dec']].groupby("SUBDIVISION").sum().plot.barh(stacked=True,figsize=(16,8));
+
+
+# ## Observations
+# - Above two graphs shows that the amount of rainfall is reasonably good in the months of march, april, may in eastern India. 
+
+# In[11]:
+
+
+plt.figure(figsize=(11,4))
+sns.heatmap(data[['Jan-Feb','Mar-May','Jun-Sep','Oct-Dec','ANNUAL']].corr(),annot=True)
+plt.show()
+
+
+# In[12]:
+
+
+plt.figure(figsize=(11,4))
+sns.heatmap(data[['JAN','FEB','MAR','APR','MAY','JUN','JUL','AUG','SEP','OCT','NOV','DEC','ANNUAL']].corr(),annot=True)
+plt.show()
+
+
+# ## Observations
+# - **Heat Map** shows the co-relation(dependency) betwenn the amounts of rainfall over months.
+# - From above it is clear that if amount of rainfall is high in the months of july, august, september then the amount of rainfall will be high annually.
+# - It is also obwserved that if amount of rainfall in good in the months of october, november, december then the rainfall is going to b good in the overall year. 
+
+# In[13]:
+
+
+#Function to plot the graphs
+def plot_graphs(groundtruth,prediction,title):        
+    N = 9
+    ind = np.arange(N)  # the x locations for the groups
+    width = 0.27       # the width of the bars
+
+    fig = plt.figure()
+    fig.suptitle(title, fontsize=12)
+    ax = fig.add_subplot(111)
+    rects1 = ax.bar(ind, groundtruth, width, color='r')
+    rects2 = ax.bar(ind+width, prediction, width, color='g')
+
+    ax.set_ylabel("Amount of rainfall")
+    ax.set_xticks(ind+width)
+    ax.set_xticklabels( ('APR', 'MAY', 'JUN', 'JUL','AUG', 'SEP', 'OCT', 'NOV', 'DEC') )
+    ax.legend( (rects1[0], rects2[0]), ('Ground truth', 'Prediction') )
+
+#     autolabel(rects1)
+    for rect in rects1:
+        h = rect.get_height()
+        ax.text(rect.get_x()+rect.get_width()/2., 1.05*h, '%d'%int(h),
+                ha='center', va='bottom')
+    for rect in rects2:
+        h = rect.get_height()
+        ax.text(rect.get_x()+rect.get_width()/2., 1.05*h, '%d'%int(h),
+                ha='center', va='bottom')
+#     autolabel(rects2)
+
+    plt.show()
+
+
+# ## Predictions
+# - For prediction we formatted data in the way, given the rainfall in the last three months we try to predict the rainfall in the next consecutive month.
+# - For all the experiments we used 80:20 training and test ratio.
+#     - Linear regression
+#     - SVR
+#     - Artificial neural nets
+# - Tersting metrics: We used Mean absolute error to train the models.
+# - We also shown the amount of rainfall actually and predicted with the histogram plots.
+# - We did two types of trainings once training on complete dataset and other with training with only telangana data
+# - All means are standard deviation observations are written, first one represents ground truth, second one represents predictions.
+# 
+
+# In[14]:
+
+
+# seperation of training and testing data
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_absolute_error
+
+division_data = np.asarray(data[['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL',
+       'AUG', 'SEP', 'OCT', 'NOV', 'DEC']])
+
+X = None; y = None
+for i in range(division_data.shape[1]-3):
+    if X is None:
+        X = division_data[:, i:i+3]
+        y = division_data[:, i+3]
+    else:
+        X = np.concatenate((X, division_data[:, i:i+3]), axis=0)
+        y = np.concatenate((y, division_data[:, i+3]), axis=0)
+        
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=42)
+
+
+# In[15]:
+
+
+#test 2010
+temp = data[['SUBDIVISION','JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL',
+       'AUG', 'SEP', 'OCT', 'NOV', 'DEC']].loc[data['YEAR'] == 2010]
+
+data_2010 = np.asarray(temp[['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL',
+       'AUG', 'SEP', 'OCT', 'NOV', 'DEC']].loc[temp['SUBDIVISION'] == 'TELANGANA'])
+
+X_year_2010 = None; y_year_2010 = None
+for i in range(data_2010.shape[1]-3):
+    if X_year_2010 is None:
+        X_year_2010 = data_2010[:, i:i+3]
+        y_year_2010 = data_2010[:, i+3]
+    else:
+        X_year_2010 = np.concatenate((X_year_2010, data_2010[:, i:i+3]), axis=0)
+        y_year_2010 = np.concatenate((y_year_2010, data_2010[:, i+3]), axis=0)
+
+
+# In[16]:
+
+
+#test 2005
+temp = data[['SUBDIVISION','JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL',
+       'AUG', 'SEP', 'OCT', 'NOV', 'DEC']].loc[data['YEAR'] == 2005]
+
+data_2005 = np.asarray(temp[['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL',
+       'AUG', 'SEP', 'OCT', 'NOV', 'DEC']].loc[temp['SUBDIVISION'] == 'TELANGANA'])
+
+X_year_2005 = None; y_year_2005 = None
+for i in range(data_2005.shape[1]-3):
+    if X_year_2005 is None:
+        X_year_2005 = data_2005[:, i:i+3]
+        y_year_2005 = data_2005[:, i+3]
+    else:
+        X_year_2005 = np.concatenate((X_year_2005, data_2005[:, i:i+3]), axis=0)
+        y_year_2005 = np.concatenate((y_year_2005, data_2005[:, i+3]), axis=0)
+
+
+# In[17]:
+
+
+#terst 2015
+temp = data[['SUBDIVISION','JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL',
+       'AUG', 'SEP', 'OCT', 'NOV', 'DEC']].loc[data['YEAR'] == 2015]
+
+data_2015 = np.asarray(temp[['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL',
+       'AUG', 'SEP', 'OCT', 'NOV', 'DEC']].loc[temp['SUBDIVISION'] == 'TELANGANA'])
+
+X_year_2015 = None; y_year_2015 = None
+for i in range(data_2015.shape[1]-3):
+    if X_year_2015 is None:
+        X_year_2015 = data_2015[:, i:i+3]
+        y_year_2015 = data_2015[:, i+3]
+    else:
+        X_year_2015 = np.concatenate((X_year_2015, data_2015[:, i:i+3]), axis=0)
+        y_year_2015 = np.concatenate((y_year_2015, data_2015[:, i+3]), axis=0)
+
+
+# In[18]:
+
+
+from sklearn import linear_model
+
+# linear model
+reg = linear_model.ElasticNet(alpha=0.5)
+reg.fit(X_train, y_train)
+y_pred = reg.predict(X_test)
+print (mean_absolute_error(y_test, y_pred))
+
+
+# In[19]:
+
+
+
+#2005
+y_year_pred_2005 = reg.predict(X_year_2005)
+
+
+#2010
+y_year_pred_2010 = reg.predict(X_year_2010)
+    
+y_year_pred_2015 = reg.predict(X_year_2015)
+
+print ("MEAN 2005")
+print (np.mean(y_year_2005),np.mean(y_year_pred_2005))
+print ("Standard deviation 2005")
+print (np.sqrt(np.var(y_year_2005)),np.sqrt(np.var(y_year_pred_2005)))
+
+
+print ("MEAN 2010")
+print (np.mean(y_year_2010),np.mean(y_year_pred_2010))
+print ("Standard deviation 2010")
+print (np.sqrt(np.var(y_year_2010)),np.sqrt(np.var(y_year_pred_2010)))
+
+
+print ("MEAN 2015")
+print (np.mean(y_year_2015),np.mean(y_year_pred_2015))
+print ("Standard deviation 2015")
+print (np.sqrt(np.var(y_year_2015)),np.sqrt(np.var(y_year_pred_2015)))
+
+
+plot_graphs(y_year_2005,y_year_pred_2005,"Year-2005")
+plot_graphs(y_year_2010,y_year_pred_2010,"Year-2010")
+plot_graphs(y_year_2015,y_year_pred_2015,"Year-2015")
+
+
+# In[ ]:
+
+
+from sklearn.svm import SVR
+
+# SVM model
+clf = SVR(gamma='auto', C=0.1, epsilon=0.2)
+clf.fit(X_train, y_train) 
+y_pred = clf.predict(X_test)
+print (mean_absolute_error(y_test, y_pred))
+
+
+# In[ ]:
+
+
+#2005
+y_year_pred_2005 = reg.predict(X_year_2005)
+
+#2010
+y_year_pred_2010 = reg.predict(X_year_2010)
+    
+#2015
+y_year_pred_2015 = reg.predict(X_year_2015)
+print ("MEAN 2005")
+print (np.mean(y_year_2005),np.mean(y_year_pred_2005))
+print ("Standard deviation 2005")
+print (np.sqrt(np.var(y_year_2005)),np.sqrt(np.var(y_year_pred_2005)))
+
+
+print ("MEAN 2010")
+print (np.mean(y_year_2010),np.mean(y_year_pred_2010))
+print ("Standard deviation 2010")
+print (np.sqrt(np.var(y_year_2010)),np.sqrt(np.var(y_year_pred_2010)))
+
+
+print ("MEAN 2015")
+print (np.mean(y_year_2015),np.mean(y_year_pred_2015))
+print ("Standard deviation 2015")
+print (np.sqrt(np.var(y_year_2015)),np.sqrt(np.var(y_year_pred_2015)))
+
+plot_graphs(y_year_2005,y_year_pred_2005,"Year-2005")
+plot_graphs(y_year_2010,y_year_pred_2010,"Year-2010")
+plot_graphs(y_year_2015,y_year_pred_2015,"Year-2015")
+
+
+# In[ ]:
+
+
+from keras.models import Model
+from keras.layers import Dense, Input, Conv1D, Flatten
+
+# NN model
+inputs = Input(shape=(3,1))
+x = Conv1D(64, 2, padding='same', activation='elu')(inputs)
+x = Conv1D(128, 2, padding='same', activation='elu')(x)
+x = Flatten()(x)
+x = Dense(128, activation='elu')(x)
+x = Dense(64, activation='elu')(x)
+x = Dense(32, activation='elu')(x)
+x = Dense(1, activation='linear')(x)
+model = Model(inputs=[inputs], outputs=[x])
+model.compile(loss='mean_squared_error', optimizer='adamax', metrics=['mae'])
+model.summary()
+
+
+# In[ ]:
+
+
+model.fit(x=np.expand_dims(X_train, axis=2), y=y_train, batch_size=64, epochs=10, verbose=1, validation_split=0.1, shuffle=True)
+y_pred = model.predict(np.expand_dims(X_test, axis=2))
+print (mean_absolute_error(y_test, y_pred))
+
+
+# In[ ]:
+
+
+#2005
+y_year_pred_2005 = reg.predict(X_year_2005)
+
+#2010
+y_year_pred_2010 = reg.predict(X_year_2010)
+    
+#2015
+y_year_pred_2015 = reg.predict(X_year_2015)
+
+print ("MEAN 2005")
+print (np.mean(y_year_2005),np.mean(y_year_pred_2005))
+print ("Standard deviation 2005")
+print (np.sqrt(np.var(y_year_2005)),np.sqrt(np.var(y_year_pred_2005)))
+
+
+print ("MEAN 2010")
+print (np.mean(y_year_2010),np.mean(y_year_pred_2010))
+print ("Standard deviation 2010")
+print (np.sqrt(np.var(y_year_2010)),np.sqrt(np.var(y_year_pred_2010)))
+
+
+print ("MEAN 2015")
+print (np.mean(y_year_2015),np.mean(y_year_pred_2015))
+print ("Standard deviation 2015")
+print (np.sqrt(np.var(y_year_2015)),np.sqrt(np.var(y_year_pred_2015)))
+
+plot_graphs(y_year_2005,y_year_pred_2005,"Year-2005")
+plot_graphs(y_year_2010,y_year_pred_2010,"Year-2010")
+plot_graphs(y_year_2015,y_year_pred_2015,"Year-2015")
+
+
+# In[ ]:
+
+
+# spliting training and testing data only for telangana
+telangana = np.asarray(data[['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL',
+       'AUG', 'SEP', 'OCT', 'NOV', 'DEC']].loc[data['SUBDIVISION'] == 'TELANGANA'])
+
+X = None; y = None
+for i in range(telangana.shape[1]-3):
+    if X is None:
+        X = telangana[:, i:i+3]
+        y = telangana[:, i+3]
+    else:
+        X = np.concatenate((X, telangana[:, i:i+3]), axis=0)
+        y = np.concatenate((y, telangana[:, i+3]), axis=0)
+        
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.01, random_state=42)        
+
+
+# In[ ]:
+
+
+from sklearn import linear_model
+
+# linear model
+reg = linear_model.ElasticNet(alpha=0.5)
+reg.fit(X_train, y_train)
+y_pred = reg.predict(X_test)
+print (mean_absolute_error(y_test, y_pred))
+
+
+# In[ ]:
+
+
+#2005
+y_year_pred_2005 = reg.predict(X_year_2005)
+
+#2010
+y_year_pred_2010 = reg.predict(X_year_2010)
+    
+#2015
+y_year_pred_2015 = reg.predict(X_year_2015)
+
+print ("MEAN 2005")
+print (np.mean(y_year_2005),np.mean(y_year_pred_2005))
+print ("Standard deviation 2005")
+print (np.sqrt(np.var(y_year_2005)),np.sqrt(np.var(y_year_pred_2005)))
+
+
+print ("MEAN 2010")
+print (np.mean(y_year_2010),np.mean(y_year_pred_2010))
+print ("Standard deviation 2010")
+print (np.sqrt(np.var(y_year_2010)),np.sqrt(np.var(y_year_pred_2010)))
+
+
+print ("MEAN 2015")
+print (np.mean(y_year_2015),np.mean(y_year_pred_2015))
+print ("Standard deviation 2015")
+print (np.sqrt(np.var(y_year_2015)),np.sqrt(np.var(y_year_pred_2015)))
+
+plot_graphs(y_year_2005,y_year_pred_2005,"Year-2005")
+plot_graphs(y_year_2010,y_year_pred_2010,"Year-2010")
+plot_graphs(y_year_2015,y_year_pred_2015,"Year-2015")
+
+
+# In[ ]:
+
+
+from sklearn.svm import SVR
+
+# SVM model
+clf = SVR(kernel='rbf', gamma='auto', C=0.5, epsilon=0.2)
+clf.fit(X_train, y_train) 
+y_pred = clf.predict(X_test)
+print (mean_absolute_error(y_test, y_pred))
+
+
+# In[ ]:
+
+
+#2005
+y_year_pred_2005 = reg.predict(X_year_2005)
+
+#2010
+y_year_pred_2010 = reg.predict(X_year_2010)
+    
+#2015
+y_year_pred_2015 = reg.predict(X_year_2015)
+
+print ("MEAN 2005")
+print (np.mean(y_year_2005),np.mean(y_year_pred_2005))
+print ("Standard deviation 2005")
+print (np.sqrt(np.var(y_year_2005)),np.sqrt(np.var(y_year_pred_2005)))
+
+
+print ("MEAN 2010")
+print (np.mean(y_year_2010),np.mean(y_year_pred_2010))
+print ("Standard deviation 2010")
+print (np.sqrt(np.var(y_year_2010)),np.sqrt(np.var(y_year_pred_2010)))
+
+
+print ("MEAN 2015")
+print (np.mean(y_year_2015),np.mean(y_year_pred_2015))
+print ("Standard deviation 2015")
+print (np.sqrt(np.var(y_year_2015)),np.sqrt(np.var(y_year_pred_2015)))
+
+plot_graphs(y_year_2005,y_year_pred_2005,"Year-2005")
+plot_graphs(y_year_2010,y_year_pred_2010,"Year-2010")
+plot_graphs(y_year_2015,y_year_pred_2015,"Year-2015")
+
+
+# In[ ]:
+
+
+model.fit(x=np.expand_dims(X_train, axis=2), y=y_train, batch_size=64, epochs=10, verbose=1, validation_split=0.1, shuffle=True)
+y_pred = model.predict(np.expand_dims(X_test, axis=2))
+print (mean_absolute_error(y_test, y_pred))
+
+
+# In[ ]:
+
+
+#2005
+y_year_pred_2005 = reg.predict(X_year_2005)
+
+#2010
+y_year_pred_2010 = reg.predict(X_year_2010)
+    
+#2015
+y_year_pred_2015 = reg.predict(X_year_2015)
+
+print ("MEAN 2005")
+print (np.mean(y_year_2005),np.mean(y_year_pred_2005))
+print ("Standard deviation 2005")
+print (np.sqrt(np.var(y_year_2005)),np.sqrt(np.var(y_year_pred_2005)))
+
+
+print ("MEAN 2010")
+print (np.mean(y_year_2010),np.mean(y_year_pred_2010))
+print ("Standard deviation 2010")
+print (np.sqrt(np.var(y_year_2010)),np.sqrt(np.var(y_year_pred_2010)))
+
+
+print ("MEAN 2015")
+print (np.mean(y_year_2015),np.mean(y_year_pred_2015))
+print ("Standard deviation 2015")
+print (np.sqrt(np.var(y_year_2015)),np.sqrt(np.var(y_year_pred_2015)))
+
+plot_graphs(y_year_2005,y_year_pred_2005,"Year-2005")
+plot_graphs(y_year_2010,y_year_pred_2010,"Year-2010")
+plot_graphs(y_year_2015,y_year_pred_2015,"Year-2015")
+
+
+# ## Prediction Observations
+# 
+# ### Training on complete dataset
+# 
+# |Algorithm|MAE|
+# |---------|---|
+# |Linear Regression|94.94821727619338|
+# |SVR|127.74073860203839|
+# |Artificial neural nets|85.2648713528865|
+# 
+# ### Training on telangana dataset
+# 
+# |Algorithm| MAE|
+# |---------|----|
+# |Linear Regression| 70.61463829282977|
+# |SVR| 90.30526775954294|
+# |Artificial neural nets |59.95190786532157|
+# 
+# - Neural Networks performs better than SVR etc.
+# - Observed MAE is very high which indicates machine learning models won't work well for prediction of rainfall.
+# - Telangana data has a single pattern that can be learned by models, rather than learning different patterns of all states. so has high accuracy.
+# - Analysed individual year rainfall patterns for 2005, 2010, 2015.
+# - Approximately close means, noticed less standard deviations.
+
+
+
+
+
